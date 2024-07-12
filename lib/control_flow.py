a3bbf9598a411ebcc9f69719300f5d3ded37a28a
Abogeerick <abogeerick@gmail.com>
2024-07-12 16:07:54 +0300

    Completed the assignment

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,47 +1,43 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    if (username == "admin" or username == "ADMIN") and password == "12345":
+    if (username=="admin" or username=="ADMIN") and password == "12345":
         return "Access granted"
-    elif username == "sudo" or password != "12345":
+    else:
         return "Access denied"
-    
 
 def hows_the_weather(temperature):
-    # your code here
-    if temperature == 33:
+    if temperature < 40:
         return "It's brisk out there!"
-    elif temperature == 55:
+    elif 40 <= temperature <= 65:
         return "It's a little chilly out there!"
-    elif temperature == 99:
+    elif temperature > 85:
         return "It's too dang hot out there!"
-    elif temperature == 75:
+    else:
         return "It's perfect out there!"
     
-
-def fizzbuzz(num):
-    # your code here
-    if num == 0 or num == 15 or num == 45:
+def fizzbuzz(number):
+    if number % 3 == 0 and number % 5 == 0:
         return "FizzBuzz"
-    elif num == 3 or num == 33 or num == 42:
+    elif number % 3 == 0:
         return "Fizz"
-    elif num == 5 or num == 10 or num == 50:
+    elif number % 5 == 0:
         return "Buzz"
-    elif num == 2 or num == 13 or num == 59:
-        return num
+    else:
+        return number
 
 def calculator(operation, num1, num2):
-    if operation == "+":
-        return num1 + num2
-    elif operation == "-":
-        return num1 - num2
-    elif operation == "*":
-        return num1 * num2
-    elif operation == "/":
-        return num1 / num2
-    else:
-        print("Invalid operation!")
-        return None
-    
-
+     if isinstance(num1, (int,float)) and isinstance(num2, (int,float)):
+        if operation == "+":
+            return num1 + num2
+        elif operation == "-":
+            return num1 - num2
+        elif operation == "*":
+            return num1 * num2
+        elif operation == "/":
+            return num1 / num2
+        else:
+            print("Invalid operation!")
+            return None
+     else:
+        raise TypeError("num1 and num2 must be integers or floats")
